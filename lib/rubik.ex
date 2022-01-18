1adabad8e7a658f83a1656829a9bba04c2f383d3
Mete Can Eris <can.eris@feyn.dev>
2022-01-18 17:51:27 +0300

    Init project

---
+++ lib/rubik.ex
@@ -0,0 +1,7 @@
+defmodule Rubik do
+  @moduledoc """
+  Rubik is a Turing-complete visual scripting language written in Elixir,
+  allowing the use of a node-based interface to create executable scripts,
+  called `algorithms`.
+  """
+end
---
+++ test/rubik_test.exs
@@ -0,0 +1,4 @@
+defmodule RubikTest do
+  use ExUnit.Case
+  doctest Rubik
+end
